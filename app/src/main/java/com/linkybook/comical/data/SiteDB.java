71adf963c51795f597759cbd5020ae14cb651306
Nathaniel Case <Qalthos@gmail.com>
2017-11-10 21:01:25 -0500

    DB migration

--- app/src/main/java/com/linkybook/comical/data/SiteDB.java
+++ app/src/main/java/com/linkybook/comical/data/SiteDB.java
@@ -1,12 +1,14 @@
 package com.linkybook.comical.data;
 
+import android.arch.persistence.db.SupportSQLiteDatabase;
 import android.arch.persistence.room.Database;
 import android.arch.persistence.room.Room;
 import android.arch.persistence.room.RoomDatabase;
 import android.arch.persistence.room.TypeConverters;
+import android.arch.persistence.room.migration.Migration;
 import android.content.Context;
 
-@Database(entities = {SiteInfo.class}, version = 1)
+@Database(entities = {SiteInfo.class}, version = 2)
 @TypeConverters({Converters.class})
 public abstract class SiteDB extends RoomDatabase {
     private static SiteDB INSTANCE;
@@ -17,7 +19,7 @@ public abstract class SiteDB extends RoomDatabase {
         if (INSTANCE == null) {
             INSTANCE =
                     Room.databaseBuilder(context.getApplicationContext(), SiteDB.class, "site")
-                            .allowMainThreadQueries()
+                            .addMigrations(MIGRATION_1_2)
                             .build();
         }
         return INSTANCE;
@@ -27,4 +29,11 @@ public abstract class SiteDB extends RoomDatabase {
         INSTANCE = null;
     }
 
+    static final Migration MIGRATION_1_2 = new Migration(1, 2) {
+        @Override
+        public void migrate(SupportSQLiteDatabase database) {
+            database.execSQL("ALTER TABLE `site` ADD visits INTEGER NOT NULL default 0");
+            database.execSQL("ALTER TABLE `site` ADD last_visit INTEGER default 0");
+        }
+    };
 }
