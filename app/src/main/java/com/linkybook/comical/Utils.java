eebd0479c92e9dd9a3dd84a8eabb411bad2cf3ea
Nathaniel Case <Qalthos@gmail.com>
2017-11-11 16:37:57 -0500

    Only protect to second level domain

--- app/src/main/java/com/linkybook/comical/Utils.java
+++ app/src/main/java/com/linkybook/comical/Utils.java
@@ -1,15 +1,23 @@
 package com.linkybook.comical;
 
+import android.text.TextUtils;
+
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.Arrays;
 
 public class Utils {
     public static String urlDomain(String url) {
+        String[] domainBits = {};
+        int l = 0;
         try {
             URI uri = new URI(url);
-            return uri.getHost();
+            domainBits = uri.getHost().split("\\.");
+            l = domainBits.length;
         } catch (URISyntaxException e) {
             return "";
         }
+        // TODO: Can't use String.join until API 26
+        return TextUtils.join(".", Arrays.copyOfRange(domainBits, l-2, l));
     }
 }
