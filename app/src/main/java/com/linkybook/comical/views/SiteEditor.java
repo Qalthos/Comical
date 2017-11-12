225ee87f70b336cb3ecdbfd0064c5c79740669a1
Nathaniel Case <Qalthos@gmail.com>
2017-11-12 15:04:19 -0500

    Add delete button to edit menu

--- app/src/main/java/com/linkybook/comical/views/SiteEditor.java
+++ app/src/main/java/com/linkybook/comical/views/SiteEditor.java
@@ -22,6 +22,7 @@ import android.arch.lifecycle.ViewModelProviders;
 import android.support.v7.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -75,6 +76,12 @@ public class SiteEditor extends AppCompatActivity {
                 }
             });
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.edit_menu, menu);
+        return true;
     }
 
     private void setupActionBar() {
